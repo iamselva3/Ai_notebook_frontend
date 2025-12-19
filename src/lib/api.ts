16c342b6960c4944622c0eefb37b23e7a1cc874b
Selva <imselva1512@gmail.com>
2025-12-19 17:57:50 +0530

    api.ts file changed

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 
 // const BASE_URL =  process.env.REACT_APP_API_URL ||"http://localhost:3001";
-const BASE_URL =import.meta.env.REACT_APP_API_URL || "http://localhost:3001";
+const BASE_URL =import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 // const BASE_URL = (process.env.REACT_APP_API_URL as string) || "http://localhost:3001";
 
